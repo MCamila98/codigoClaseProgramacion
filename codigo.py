ce76e56a72bad9d40a4bc9f56553bb53807bf141
María Camila <poccam555@gmail.com>
2024-05-09 15:58:20 -0500

    primer codigo

---
+++ codigo.py
@@ -0,0 +1,8 @@
+nombre= "camila"
+apellido =" cabrera"
+
+
+resultado=("mi nombre es ",nombre,apellido)
+print("mi edad es ")
+print(resultado)
+print("mi nombre es ",nombre,"mi apellido es",apellido)
