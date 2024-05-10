6438473a9fcfec76b9ef9f6663d8761e873e3d69
María Camila <poccam555@gmail.com>
2024-05-09 19:19:11 -0500

    agregar edad codigo.py

--- codigo.py
+++ codigo.py
@@ -5,4 +5,4 @@ apellido =" cabrera"
 resultado=("mi nombre es ",nombre,apellido)
 print("mi edad es ")
 print(resultado)
-print("mi nombre es ",nombre,"mi apellido es",apellido)
+print("mi nombre es ",nombre,"mi apellido es 15",apellido)
